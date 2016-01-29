f28196bf6aec3ae8b51148cd09f3ac40daf5a6f6
Tom Wright <tom.w.wright@live.com>
2016-01-30 01:00:00 +1100

    - Basic Testing Scene (TestScene.scene) - Basic Movement Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    public float moveSpeed = 5f;
+    
+    private CharacterController charController;
+	// Use this for initialization
+	void Start () {
+	   charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   if(Input.GetKey(KeyCode.W))
+       {
+           charController.Move(Vector3.forward * moveSpeed * Time.deltaTime);
+       }
+       if(Input.GetKey(KeyCode.S))
+       {
+           charController.Move(Vector3.back * moveSpeed * Time.deltaTime);
+       }
+       if(Input.GetKey(KeyCode.A))
+       {
+           charController.Move(Vector3.left * moveSpeed * Time.deltaTime);
+       }
+       if(Input.GetKey(KeyCode.D))
+       {
+           charController.Move(Vector3.right * moveSpeed * Time.deltaTime);
+       }
+	}
+}
