ca18c21700b062d679a3fe9e9c5396c5020a8a73
Sampson Oliver <sampsonjoliver@outlook.com>
2016-01-30 11:59:58 +1100

    Implemented Pleb skeletons and conversion/movement scripts

---
+++ Assets/Scripts/pleb/PlebConversion.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PlebConversion : MonoBehaviour {
+    public static float MIN_CONVERSION = 0;
+    public static float MAX_CONVERSION = 100;
+    public float CONVERSION_FALLOFF_TIME = 4f;
+    public float conversionFalloffElapsedTime;
+    public float conversionPerc;
+    
+    public List<GameObject> availableConversionTargets;
+    public GameObject conversionTarget;
+    
+	// Use this for initialization
+	void Start () {
+	   conversionFalloffElapsedTime = 0f;
+	   conversionPerc = 0f;
+       availableConversionTargets = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   UpdateTargets();
+	}
+    
+    private void UpdateTargets() {
+        if (conversionTarget != null) {
+            // If we have a target but we are not in its influence sphere            
+            if (!availableConversionTargets.Contains(conversionTarget)) {
+                if (HasAvailableConversionTargets()) {
+                    // Check if there is a secondary target we can switch to
+                    ConvertToNextTarget();
+                } else {
+                    // We aren't in any influence sphere, so we need to do conversion falloff
+                    conversionFalloffElapsedTime += Time.deltaTime;
+                    conversionPerc = Mathf.Lerp(MAX_CONVERSION, MIN_CONVERSION, conversionFalloffElapsedTime / CONVERSION_FALLOFF_TIME);
+                    Debug.Log("Current conversion = " + conversionPerc);
+                    
+                    if (conversionPerc <= 0f) {
+                        Debug.Log("Pleb conversion falling off.");
+                        conversionTarget = null;
+                    }
+                }
+            }
+        }
+    }
+    
+    public void OnEnterConversionSphere(GameObject target) {
+        // Add the new available target to the list of available targets
+        if (!availableConversionTargets.Contains(target))
+            availableConversionTargets.Add(target);
+       
+        ConvertToNextTarget();
+    }
+    
+    public void OnExitConversionSphere(GameObject target) {
+        // Remove the target from the list of available targets
+        availableConversionTargets.Remove(target);
+        
+        // Set the target to the next available target if one exists
+        ConvertToNextTarget();
+    }
+    
+    public bool HasAvailableConversionTargets() {
+        return availableConversionTargets.Count > 0;
+    }
+    
+    public GameObject GetNextAvailableConversionTarget() {
+        if (HasAvailableConversionTargets())
+            return availableConversionTargets[0];
+        else
+            return null;
+    }
+    
+    private void ConvertToNextTarget() {
+        if (HasAvailableConversionTargets()) {
+            Debug.Log("Pleb acquiring new target " + GetNextAvailableConversionTarget());
+            conversionTarget = GetNextAvailableConversionTarget();
+            conversionFalloffElapsedTime = 0f;
+            conversionPerc = MAX_CONVERSION;
+        }
+    }
+}
---
+++ Assets/Scripts/pleb/PlebMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlebMovement : MonoBehaviour {
+    
+    public float moveSpeed = 5f;
+    private PlebConversion plebConversion;
+    private CharacterController charController;
+    
+	// Use this for initialization
+	void Start () {
+	   plebConversion = GetComponent<PlebConversion>();
+	   charController = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (plebConversion.conversionTarget != null) {
+            MoveToTarget(plebConversion.conversionTarget.gameObject.transform.position);
+        }
+	}
+    
+    private void MoveToTarget(Vector3 targetPos) {
+        Vector3 moveDir = Vector3.Normalize(targetPos - this.transform.position);
+        charController.Move(moveSpeed * moveDir * Time.deltaTime);
+    } 
+}
